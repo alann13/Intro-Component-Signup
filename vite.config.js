4b34aaba93cb147efde2951064f7212338ed0d4d
Alan <alan.nguy@outlook.com>
2021-12-02 22:57:37 -0500

    feature: markup and styling added

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import autoprefixer from 'autoprefixer'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
+})
