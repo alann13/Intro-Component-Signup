380ff7deab6194289035d94ac03d9d205c581773
Alan <alan.nguy@outlook.com>
2021-12-03 00:02:08 -0500

    feature: added small html form validation with minor styling refactoring

---
+++ main.js
@@ -0,0 +1,11 @@
+// UI
+const form = document.getElementById('signup-form')
+const firstNameInput = document.getElementById('first-name')
+const lastNameInput = document.getElementById('last-name')
+const emailInput = document.getElementById('email-address')
+const passwordInput = document.getElementById('password')
+const claimButton = document.getElementById('claim-btn')
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+})
